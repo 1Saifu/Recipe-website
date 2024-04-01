eb19cb5c261da56622edcfa710e24c20696b1b3a
1saifu <saifukhan@hotmail.se>
2024-04-01 17:33:43 +0200

    created functions

--- recipe/controllers/creator.controller.js
+++ recipe/controllers/creator.controller.js
@@ -1,11 +1,35 @@
 const User = require('../models/user');
 
 async function registerUser(req, res) {
-
+    const { email, password } = req.body;
+    try{ 
+        const existingUser = await User.findOne({ email });
+        if(existingUser) {
+            return res.status(400).json({ error: 'User already exists' });
+        }
+        //Here we create a user
+        const newUser = await User.create({ email, password });
+        res.status(201).json(newUser);
+    } catch(error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
 }
 
 async function loginUser(req, res) {
-    
+    const { email, password } = req.body;
+    try{ 
+        const user = await User.findOne({ email });
+        if(!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        if(password !== user.password) {
+            return res.status(401).json({ error: 'Incorrect password' });
+        }
+        res.json(user);
+    } catch(error) {
+    res.status(500).json({ error: 'Internal server error' });
+    }
 }
 
 module.exports = {
--- recipe/controllers/recipe.controller.js
+++ recipe/controllers/recipe.controller.js
@@ -1,23 +1,61 @@
 const Recipe = require('../models/recipe')
 
 async function getAllRecipes(req, res) {
-    
+    try {
+        const recipes = await Recipe.find();
+        res.json(recipes);
+    } catch(error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
 }
 
 async function getRecipeById(req, res) {
-    
+    const { id } = req.params;
+    try {
+        const recipe = await Recipe.findById(id);
+        if(!recipe) {
+            res.status(404).json({ error: 'Recipe not found' })
+        }
+    } catch(error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
 }
 
 async function createRecipe(req, res) {
-    
+    const { title, ingredients, instructions }= req.body;
+    try {
+        const recipe = await Recipe.create({ title, ingredients, instructions });
+        res.status(201).json(recipe)
+    } catch(error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
 }
 
 async function updateRecipe(req, res) {
-    
+    const { id } = req.params;
+    const { title, ingredients, instructions }= req.body;
+    try {
+        const updatedRecipe = await Recipe.findByIdAndUpdate(id, { title, ingredients, instructions }, { new: true });
+        if(!updatedRecipe) {
+            res.status(404).json({ error: 'Recipe not found' })
+        }
+        res.json(updatedRecipe);
+    } catch(error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
 }
 
 async function deleteRecipe(req, res) {
-    
+    const { id } = req.params;
+    try {
+        const deleteRecipe = await Recipe.findByIdAndDelete(id);
+        if(!deleteRecipe) {
+            res.status(404).json({ error: 'Recipe not found' })
+        }
+        res.json({ message: 'Recipe successfully deleted' })
+    } catch(error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
 }
 
 async function favoriteRecipe(req, res) {
