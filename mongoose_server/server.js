dbd75edd1daef74eb38bcba8d409e15535a919df
1saifu <saifukhan@hotmail.se>
2024-04-05 12:25:04 +0200

    Database created

--- mongoose_server/server.js
+++ mongoose_server/server.js
@@ -1,46 +1,18 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(cors({
     origin: [`http://localhost:3000`, `http://127.0.0.1:3000`]
 }))
-
-const uri = "mongodb://127.0.0.1:27017/recipe-web"
-
-mongoose.connect(uri)
-.then(() => {
-    console.log("Connected to MongoDB");
-})
-.catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-})
-
-
-const User = require("../recipe/models/user");
-const Recipe = require("../recipe/models/recipe");
-
-const { registerUser, loginUser } = require("../recipe/controllers/creator.controller");
-const { 
-    getAllRecipes, 
-    getRecipeById, 
-    createRecipe, 
-    updateRecipe, 
-    deleteRecipe, 
-    favoriteRecipe, 
-    createReview } = require("../recipe/controllers/recipe.controller");
-
-const creatorRouter = require('../recipe/routes/creator.route');
-const recipeRouter = require('../recipe/routes/recipe.route');
-
-app.use("/creator", creatorRouter);
-app.use("/recipe", recipeRouter);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+module.exports = app;
---
+++ recipe/index.js
@@ -0,0 +1,34 @@
+const app = require("../mongoose_server/server");
+const mongoose = require("mongoose");
+
+const uri = "mongodb://127.0.0.1:27017/web-recipe";
+
+const { registerUser, loginUser } = require("../recipe/controllers/creator.controller");
+const { 
+    getAllRecipes, 
+    getRecipeById, 
+    createRecipe, 
+    updateRecipe, 
+    deleteRecipe, 
+    favoriteRecipe, 
+    createReview } = require("../recipe/controllers/recipe.controller");
+
+const User = require("../recipe/models/user");
+const Recipe = require("../recipe/models/recipe");
+
+const creatorRouter = require('../recipe/routes/creator.route');
+const recipeRouter = require('../recipe/routes/recipe.route');
+
+app.use("/creator", creatorRouter);
+app.use("/recipe", recipeRouter);
+
+async function connectToDatabase() {
+    try {
+      await mongoose.connect(uri);
+      console.log("Connected to MongoDB");
+    } catch (error) {
+      console.error("Error connecting to MongoDB:", error);
+    }
+  }
+  
+  connectToDatabase();
