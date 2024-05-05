17964b0ea873f5efefaeb692057906e4b6fa0ef3
1saifu <saifukhan@hotmail.se>
2024-05-05 20:38:09 +0200

    fixed

--- recipe-web/src/firebase.js
+++ recipe-web/src/firebase.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
@@ -16,7 +15,6 @@ const firebaseConfig = {
   
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-const imgDB = getStorage(app)
+  const imgDB = getStorage(app)
 
 export {imgDB};
