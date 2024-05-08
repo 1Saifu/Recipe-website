82ca94e9e61effee28087be01ff2393d8a1180a8
1saifu <saifukhan@hotmail.se>
2024-05-08 15:57:35 +0200

    fixed server

--- mongoose_server/server.js
+++ mongoose_server/server.js
@@ -7,11 +7,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin: [`http://localhost:3000`, `http://127.0.0.1:3000`,]
+    origin: [`http://localhost:3000`, `http://127.0.0.1:3000`]
 }))
 
 const PORT = process.env.PORT || 8080;
-app.listen(() => {
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
 
